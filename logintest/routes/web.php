92446b236e3506c5a832a5a23e5c7edff033f0fa
bbug02081702 <yuntips1702@gmail.com>
2022-11-13 23:22:14 +0700

    login

---
+++ logintest/routes/web.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+use App\Http\Controllers\SinhvienController;
+use App\Http\Controllers\CustomAuthController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return Inertia::render('Welcome', [
+//         'canLogin' => Route::has('login'),
+//         'canRegister' => Route::has('register'),
+//         'laravelVersion' => Application::VERSION,
+//         'phpVersion' => PHP_VERSION,
+//     ]);
+// });
+
+// Route::middleware([
+//     'auth:sanctum',
+//     config('jetstream.auth_session'),
+//     'verified',
+// ])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return Inertia::render('Dashboard');
+//     })->name('dashboard');
+//     Route::resource('/sinhvien',SinhvienController::class);
+// });
+Route::get('/', [CustomAuthController::class, 'home']); 
+Route::get('dashboard1', [CustomAuthController::class, 'dashboard1']); 
+Route::get('login', [CustomAuthController::class, 'index'])->name('login');
+Route::post('postlogin', [CustomAuthController::class, 'login'])->name('postlogin'); 
+Route::get('signup', [CustomAuthController::class, 'signup'])->name('register-user');
+Route::post('postsignup', [CustomAuthController::class, 'signupsave'])->name('postsignup'); 
+Route::get('signout', [CustomAuthController::class, 'signOut'])->name('signout');
+
+
